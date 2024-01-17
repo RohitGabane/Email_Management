25eb9d74a9f8f3e038d216e91df546a5e52c4b51
Amazatic <Amazatic@DESKTOP-OOM2M0F>
2024-01-17 11:40:00 +0530

    Add project files.

---
+++ Email_Management/EmailService.cs
@@ -0,0 +1,48 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using System;
+
+namespace Email_Management
+{
+    public class EmailService
+    {
+        private readonly string smtpServer;
+        private readonly int smtpPort;
+        private readonly string smtpUsername;
+        private readonly string smtpPassword;
+
+        public EmailService(string smtpServer, int smtpPort, string smtpUsername, string smtpPassword)
+        {
+           this.smtpServer = smtpServer;
+            this.smtpPort = smtpPort;
+            this.smtpUsername = smtpUsername;
+            this.smtpPassword = smtpPassword;
+        }
+
+        public void SendEmail(string toEmail, string subject, string body)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Rohit", smtpUsername));
+            message.To.Add(new MailboxAddress("", toEmail));
+            message.Subject = subject;
+
+            var textPart = new TextPart("plain")
+            {
+                Text = body
+            };
+
+            var multipart = new Multipart("mixed");
+            multipart.Add(textPart);
+            message.Body = multipart;
+
+            using (var client = new MailKit.Net.Smtp.SmtpClient())
+            {
+                client.Connect(smtpServer, smtpPort, SecureSocketOptions.StartTls);
+                client.Authenticate(smtpUsername, smtpPassword);
+                client.Send(message);
+                client.Disconnect(true);
+            }
+        }
+    }
+}
---
+++ Email_Management/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+namespace Email_Management
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string smtpServer = "smtp.gmail.com";
+            int smtpPort = 587;
+            string smtpUsername = "rohitgabane1234@gmail.com";
+            string smtpPassword = "fzir fvrv rjnf xtzq";
+
+            var emailService = new EmailService(smtpServer, smtpPort, smtpUsername, smtpPassword);
+
+            string toEmail = "grohit0923@gmail.com"; //recepient email
+            string subject = "Task Completion Email!!!!!!!!!!!!!!!";
+            string body = "Ronny is here......Gud Morning Sir!!!!!!!!!!!";
+
+            emailService.SendEmail(toEmail, subject, body);
+            Console.WriteLine("Email sent successfully!");
+        }
+    }
+}
